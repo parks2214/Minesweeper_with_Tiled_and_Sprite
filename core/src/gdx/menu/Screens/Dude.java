08c6dd7c5755fd2a2cf4d1d1c6a14cc858a63f7a
hallk1331 <hallk1331@FHC-IC0034079.wrdsb.ca>
2017-12-05 13:06:05 -0500

    Wall class working

---
+++ core/src/gdx/menu/Screens/Dude.java
@@ -0,0 +1,13 @@
+package gdx.menu.Screens;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.Gdx;
+
+public class Dude extends Sprite{
+    public Dude(int nW, int nH, int nX, int nY){
+        super(new Texture(Gdx.files.internal("boye.jpg")));
+        setSize(nW, nH);
+        setPosition(nX, nY);
+    }
+}
